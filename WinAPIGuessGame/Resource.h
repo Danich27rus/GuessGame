6c529cbab48b2d6cd31fceac4505d457116f809c
Danich27rus <grighoriev_daniil@mail.ru>
2023-03-23 14:48:08 +0700

    initial release

---
+++ WinAPIGuessGame/Resource.h
@@ -0,0 +1,43 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется WinAPIGuessGame.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_WINAPIGUESSGAME_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_WINAPIGUESSGAME		107
+#define IDI_SMALL				108
+#define IDC_WINAPIGUESSGAME		109
+#define IDC_MYICON				2
+#define IDC_ROLL				3   //кнопка "Сгенерировать число"
+#define IDC_NUMBERCHECK			4	//кнопка "Проверка числа"
+#define IDC_RESTART				5	//кнопка "Начать с начала"
+#define IDC_HINT				1	//чекбокс "Подсказка"
+#define IDC_GROUPBOX			10  //TODO: возможные ошибки т.к. два гроупбокса с одним IDC
+#define IDC_COMBOBOX			15  //TODO: возможные ошибки т.к. два гроупбокса с одним IDC
+#define IDC_INPUTTEXTBOX		20  //InputBox для числа
+#define IDC_INPUTBOXFROM		21	//От 
+#define IDC_INPUTBOXTO			22	//До
+#define IDC_INPUTBOXBET			23	//Ставка
+#define IDC_OUTPUTTEXTBOX		25	//OutputBox для числа
+#define IDC_INPUTBET			24
+//#define
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Следующие стандартные значения для новых объектов
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
